8e7f4046dd742b3a00b5208f89e22d31bcb588af
rockerBOO <rockerboo@gmail.com>
2015-07-22 23:18:41 -0400

    Fixed some more colors

--- lib/view_helper.ex
+++ lib/view_helper.ex
@@ -12,15 +12,9 @@ defmodule Personal.ViewHelper do
   def active_top_level(options, conn) when is_list(options)  do
     current_path = Path.join(["/" | conn.path_info])
 
-    IO.inspect current_path
-    IO.inspect options
-
     matched = Enum.filter(options, fn (option) ->
       option == current_path
     end)
-
-    IO.puts "matched"
-    IO.inspect matched
 
     if length(matched) > 0 do
       " class=\"mui-active\""
@@ -31,9 +25,6 @@ defmodule Personal.ViewHelper do
 
   def active_top_level(option, conn) do
     current_path = Path.join(["/" | conn.path_info])
-
-    IO.inspect current_path
-    IO.inspect option
 
     if option == current_path do
       " class=\"mui-active\""
